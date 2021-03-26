3130d27b3f5400bc51f1b9cdce1da977039dcf9e
Thiago-Nascimento <thiago.r.nascimento@hotmail.com>
2021-03-26 16:54:13 -0300

    Server sendo selecionado dinamicamente, envio de arquivo feito

--- index.js
+++ index.js
@@ -1,49 +1,60 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
+const fs = require('fs');
 
 const client = new Discord.Client();
 
 const prefix = "!";
 
 client.on("message", (message) => {
-    const server = client.guilds.cache.get("824279131617230869");
-    
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
-
+    
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(" ");
     const command = args.shift().toLowerCase();
+    
+    const server = client.guilds.cache.get(message.guild.id);
 
     if (command === "ping") {
         const timeTaken = Date.now() - message.createdTimestamp;
         message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
     } else if (command === "chamada") {
-        const canalManha = "824295677835739187";
-        const canalTarde = "824299387382923345";
-        
-        let turno;
+        const argCanal = args[0];
+
         let canal;
-        let presentes;
+        let membros;
+        let presentes = [];
 
-        const agora = new Date;
-    
-        if (agora.getHours() >= 5 && agora.getHours() < 12) { turno = "manha" }
-        else if (agora.getHours() >= 12 && agora.getHours() < 18) { turno = "tarde" }
-        
-        if (turno === "manha") {
-            canal = server.channels.cache.get(canalManha);
-        } else if (turno === "tarde") {
-            canal = server.channels.cache.get(canalTarde);
+        if (argCanal) {
+            canal = server.channels.cache.get(argCanal);
         }
 
-        presentes = canal.members;
+        membros = canal.members;
+        console.log(membros.length);
 
-        for ([memberID, member] of presentes) {
+        fs.open('chamada.txt', 'w', (err) => {
+            if (err) throw err;
+            console.log("File Created!!!");
+        })
+
+        for ([memberID, member] of membros) {
             console.log(member.displayName);
-            message.reply(member.displayName)
+            message.reply(member.displayName);
+
+            presentes.push(member.displayName);
         }
+
+        presentes.sort();
+
+        for (presente of presentes) {
+            fs.appendFile('chamada.txt', presente + "\n", (err) => {
+                if (err) throw err;
+                console.log("File Updated!!");
+            })
+        }       
         
+        message.channel.send("Olha o arquivo aí", { files: ["./chamada.txt"] });
     }
 });
 
